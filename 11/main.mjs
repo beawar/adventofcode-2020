7b357d2b8deff90a7510c219ae93154c1ef1f664
Beatrice Guerra <betrix.pongo@gmail.com>
2020-12-11 23:43:50 +0100

    Day 11 part 2 and refactor of part 1 to use same functions

--- 11/main.mjs
+++ 11/main.mjs
@@ -17,62 +17,103 @@ const fillData = (file) => {
     });
 }
 
-fillData('input.txt')
-.then(data => {
+// return the number of adjacent seats occupied around current one, with a range limit to look around equals to limit param.
+// if no limit is given, than there is no range, and adjacent could be any seat visible
+const getAdjacentOccupiedSeats = (fromData, currentCol, currentRow, limit) => {
+    // save the visible seat for every direction
+    let adjacent = {
+        upLeft: undefined,
+        up: undefined,
+        upRight: undefined,
+        left: undefined,
+        right: undefined,
+        downLeft: undefined,
+        down: undefined,
+        downRight: undefined
+    };
+
+    // look around at most for limit seats around current
+    // if limit is undefined, it is equal to the max dimension between rows and column
+    if (limit == null) {
+        limit = Math.max(fromData.length, fromData[currentRow].length);
+    }
+    
+    for (let i=1; i<=limit; i++) {
+        // check elements to the left, if not already found
+        if (adjacent.left === undefined && currentCol-i>=0 && fromData[currentRow][currentCol-i] !== '.') {
+            adjacent = {...adjacent, left: fromData[currentRow][currentCol-i]};
+        }
+        // check elements to the right, if not already found
+        if (adjacent.right === undefined && currentCol+i<fromData[currentRow].length && fromData[currentRow][currentCol+i] !== '.'){
+            adjacent = {...adjacent, right: fromData[currentRow][currentCol+i]};
+        }
+        // check elements above
+        if (currentRow - i >= 0) {
+            // directly above
+            if (adjacent.up === undefined && fromData[currentRow-i][currentCol] !== '.') {
+                adjacent = {...adjacent, up: fromData[currentRow-i][currentCol]};
+            }
+            
+            // diagonally to left
+            if (adjacent.upLeft === undefined && currentCol-i>=0 && fromData[currentRow-i][currentCol-i] !== '.') {
+                adjacent = {...adjacent, upLeft: fromData[currentRow-i][currentCol-i]};
+            }
+            
+            // diagonally to right
+            if (adjacent.upRight === undefined && currentCol+i<fromData[currentRow-i].length && fromData[currentRow-i][currentCol+i] !== '.') {
+                adjacent = {...adjacent, upRight: fromData[currentRow-i][currentCol+i]};
+            }
+        }
+
+        // check elements under
+        if (currentRow + i < fromData.length) {
+            // directly under
+            if (adjacent.down === undefined && fromData[currentRow+i][currentCol] !== '.') {
+                adjacent = {...adjacent, down: fromData[currentRow+i][currentCol]};
+            }
+            
+            // diagonally to left
+            if (adjacent.downLeft === undefined && currentCol-i>=0 && fromData[currentRow+i][currentCol-i] !== '.') {
+                adjacent = {...adjacent, downLeft: fromData[currentRow+i][currentCol-i]};
+            }
+            
+            // diagonally to right
+            if (adjacent.downRight === undefined && currentCol+i<fromData[currentRow+i].length && fromData[currentRow+i][currentCol+i] !== '.') {
+                adjacent = {...adjacent, downRight: fromData[currentRow+i][currentCol+i]};
+            }
+        }
+    }
+
+    return Object.values(adjacent).filter(seat => seat === '#').length;
+    
+}
+
+const movePeople = (data, occupiedToFreeLimit, rangeLimit) => {
     let stable = false;
     let firstRound = true;
+    let i=0;
     while(!stable) {
         const prevData = [...data];
-        //console.log('-------------------------------------------------------------------------');
-        //console.log('-------------------------------------------------------------------------');
+        
         for(let i=0; i<data.length; i++) {
-            const prevPrevLine = i>0 ? prevData[i-1] : null;
-            const prevNextLine = i+1<prevData.length ? prevData[i+1] : null;
-            const prevCurrLine = prevData[i];
             for(let j=0; j<data[i].length; j++) {
                 // for every seat in the line, put in the array adjacent all the 8 adjacent seats
-                const adjacent = [];
-                if (j>0) {
-                    adjacent.push(prevCurrLine[j-1]);
-                }
-                if (j+1<data[i].length){
-                    adjacent.push(prevCurrLine[j+1]);
-                }
-                if (prevPrevLine) {
-                    adjacent.push(prevPrevLine[j]);
-                    if (j>0) {
-                        adjacent.push(prevPrevLine[j-1]);
-                    }
-                    if (j+1<data[i].length){
-                        adjacent.push(prevPrevLine[j+1]);
-                    }
-                }
-                if (prevNextLine) {
-                    adjacent.push(prevNextLine[j]);
-                    if (j>0) {
-                        adjacent.push(prevNextLine[j-1]);
-                    }
-                    if (j+1<data[i].length){
-                        adjacent.push(prevNextLine[j+1]);
-                    }
-                }
+                const adjacent = getAdjacentOccupiedSeats(prevData, j, i, rangeLimit);
 
-                //console.log(i, j, data[i][j], '[', adjacent.join(','), ']', adjacent.filter(seat => seat === '#').length);
-                if (data[i][j] === 'L') {
+                if (prevData[i][j] === 'L') {
                     // if the seat is empty and there are no occupied seats adjacent to it, the seat becomes occupied
-                    if(adjacent.filter(seat => seat === '#').length === 0) {
+                    if(adjacent === 0) {
                         data[i] = data[i].replaceAt(j, '#');
                     }
                 }
-                else if (data[i][j] === '#') {
+                else if (prevData[i][j] === '#') {
                     // if the seat is occupied and four or more seats adjacent to it are occupied, the seat becomes empty
-                    if (adjacent.filter(seat => seat === '#').length >= 4) {
+                    if (adjacent >= occupiedToFreeLimit) {
                         data[i] = data[i].replaceAt(j,'L');
                     }
                 }
                 // floor never changes
             }
-            //console.log(i, data[i]);
         }
         // once done all the changes, check if current data equals to prev data. If so, state is stable
         if (!firstRound && data.join('') === prevData.join('')) {
@@ -81,7 +122,15 @@ fillData('input.txt')
         firstRound = false;
     }
 
-    //console.log(data.join(''));
-    console.log((data.join('').match(/#/g) || []).length);
+    return (data.join('').match(/#/g) || []).length;
+}
+
+fillData('input.txt')
+.then(data => {
+    const totOccupiedSeats1 = movePeople([...data], 4, 1);
+    const totOccupiedSeats2 = movePeople([...data], 5);
+
+
+    console.log(totOccupiedSeats1, totOccupiedSeats2);
 })
 .catch(err => console.log(err));
