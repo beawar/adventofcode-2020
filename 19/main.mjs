3b7782082e0850bd0c3abfae884c1d13a5c91323
Beatrice Guerra <betrix.pongo@gmail.com>
2020-12-20 01:03:05 +0100

    Day 19 part 2

--- 19/main.mjs
+++ 19/main.mjs
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import { resolve } from 'path';
 import readline from 'readline';
 
 function fillData(file) {
@@ -23,7 +22,7 @@ function fillData(file) {
     });
 }
 
-function resolveRule(rules, number) {
+function resolveRule(rules, number, repetitions) {
     let rule = rules[number];
     //console.log(number, rule);
     // for part 2 replace rule itself inside rule
@@ -31,13 +30,20 @@ function resolveRule(rules, number) {
     if ((rule.match(numberRegex) || []).length > 0) {
         const ruleWithoutNumberArr = rule.split(number);
         let ruleWithoutNumber = '';
-        for (let rulePart of ruleWithoutNumberArr) {
-            if (rulePart.trim().length > 0) {
-                ruleWithoutNumber += ' ' + rulePart.trim() + ' + ';
-            }  
+        if (ruleWithoutNumberArr[ruleWithoutNumberArr.length-1].trim().length === 0) {
+            ruleWithoutNumber = rule.replace(numberRegex, '+');
         }
+        else {
+            for (let rulePart of ruleWithoutNumberArr) {
+                if (rulePart.trim().length > 0) {
+                    ruleWithoutNumber += ' ' + rulePart.trim() + '{' + repetitions + '}';
+                }  
+            }
+        }
+        
+        
         console.log('before', rule);
-        rule = rule.replace(numberRegex, '('+ruleWithoutNumber+')+');
+        rule = rule.replace(numberRegex, '('+ruleWithoutNumber+')');
         console.log('after', rule);
         //console.log(number, rule.replace(/\s+/g, ''));
     }
@@ -47,7 +53,7 @@ function resolveRule(rules, number) {
         if (element && element.trim().length > 0 && !isNaN(element.trim())
             // for part 2 avoid replacement of rule itself
             && parseInt(element) !== number) {
-            let extendedRule = resolveRule(rules, parseInt(element));
+            let extendedRule = resolveRule(rules, parseInt(element), repetitions);
             if (extendedRule.includes('|')) {
                 extendedRule = '(' + extendedRule +')';
             }
@@ -63,19 +69,46 @@ function resolveRule(rules, number) {
     return rule;
 }
 
-function main(rules, messages) {
-    const extendedRule0 = resolveRule(rules, 0);
+function main(rules, messages, repetitions) {
+    const extendedRule0 = resolveRule(rules, 0, repetitions);
     const regexRule0 = '\\b' + extendedRule0.replace(/\s+/g, '') + '\\b';
     //console.log(regexRule0);
     const rule0Matches = messages.filter(msg => msg.match(new RegExp(regexRule0)));
-    console.log(rule0Matches.length);
+    console.log(repetitions, rule0Matches.length);
+    return rule0Matches.length;
 }
 
 fillData('input.txt')
 .then(({ rules, messages }) => {
     // part 1 
-    main({...rules}, messages);
+    const part1 = main({...rules}, messages);
     // part 2
-    const rules2 = {...rules, 8: ' 42 | 42 8', 11: ' 42 31 | 42 11 31'}
-    main(rules2, messages); 
+    const rules2 = {...rules, 8: ' 42 | 42 8', 11: ' 42 31 | 42 11 31'};
+
+    let res = 0;
+    const baseRes = main({...rules2}, messages, 0);
+    res += baseRes;
+    for (let i=0; i<10; i++) {
+        res += main({...rules2}, messages, i+1) - baseRes;
+    }
+    console.log('one mode', res);
+    let res2 = 0
+    for (let i=0; i<10; i++) {
+        res2 += main2({...rules2}, messages, i+1);
+    }
+    console.log('other mode', res2)
+
 })
+
+
+function main2(rules, messages, repetitions) {
+    const extendedRule42 = resolveRule(rules, 42);
+    const extendedRule31 = resolveRule(rules, 31);
+    
+    const extendedRule0 = `(${extendedRule42})+(${extendedRule42}){${repetitions}}(${extendedRule31}){${repetitions}}`;
+    const regexRule0 = '\\b' + extendedRule0.replace(/\s+/g, '') + '\\b';
+    //console.log(regexRule0);
+    const rule0Matches = messages.filter(msg => msg.match(new RegExp(regexRule0)));
+    console.log(rule0Matches.length);
+    return rule0Matches.length;
+}
